51f61bb4e4b1bec1f1ef22a403925ad73eac8c26
Tiny <corfer@yeah.net>
2026-04-16 13:35:37 +0800

    chat server module

--- server/app/index.ts
+++ server/app/index.ts
@@ -14,6 +14,7 @@ import { modelController } from "../modules/model/model.controller";
 import { usageController } from "../modules/usage/usage.controller";
 import { accountController } from "../modules/account/account.controller";
 import { roleController } from "../modules/role/role.controller";
+import { chatSessionController, chatMessageController } from "../modules/chat/chat.controller";
 
 const PORT = parseInt(process.env.SERVER_PORT || "3300");
 initialize();
@@ -31,6 +32,8 @@ Bun.serve({
             usageController,
             accountController,
             roleController,
+            chatSessionController,
+            chatMessageController,
         ]);
         if (apiResponse) return apiResponse;
         const staticResponse = await mountstatic(staticPath, pathName);
