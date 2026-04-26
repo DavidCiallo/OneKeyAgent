a4e3677f5455a65d403a75efed85e16368655b68
Tiny <corfer@yeah.net>
2026-04-26 17:16:32 +0800

    person usage stat

--- shared/modules/usage/usage.router.ts
+++ shared/modules/usage/usage.router.ts
@@ -1,5 +1,5 @@
 import { BaseRouterInstance } from "../../lib/default/decorator";
-import { UsageListRequest, UsageListResponse, UsageStatsRequest, UsageStatsResponse } from "./usage.interface";
+import { UsageListRequest, UsageListResponse, UsageStatsRequest, UsageStatsResponse, MyUsageRequest, MyUsageResponse } from "./usage.interface";
 
 export class UsageRouterInstance extends BaseRouterInstance {
     base = "/api";
@@ -7,14 +7,17 @@ export class UsageRouterInstance extends BaseRouterInstance {
     router = [
         { path: "/list", handler: Function },
         { path: "/stats", handler: Function },
+        { path: "/mystats", handler: Function },
     ];
 
     list!: (query: UsageListRequest) => Promise<UsageListResponse>;
     stats!: (query: UsageStatsRequest) => Promise<UsageStatsResponse>;
+    mystats!: (query: MyUsageRequest) => Promise<MyUsageResponse>;
 
     constructor(inject: Function, functions?: {
         list: (query: UsageListRequest) => Promise<UsageListResponse>,
         stats: (query: UsageStatsRequest) => Promise<UsageStatsResponse>,
+        mystats: (query: MyUsageRequest) => Promise<MyUsageResponse>,
     }) {
         super();
         inject(this, functions);
