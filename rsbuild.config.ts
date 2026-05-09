883ed7933090f6e1bf4037a1b247613afd338f4e
Tiny <corfer@yeah.net>
2026-05-10 00:58:03 +0800

    feat: unify name

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,7 @@ import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
     html: {
-        title: "HEX.AI",
+        title: "EHEX",
         favicon: "./public/favicon.svg",
     },
     plugins: [pluginReact()],
