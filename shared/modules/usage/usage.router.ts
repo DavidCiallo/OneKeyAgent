4fe9911d21055a7e8e6857ac3dae487b1ea0174e
Tiny <corfer@yeah.net>
2026-05-11 19:11:53 +0800

    feat: update usage module shared schema, entity, interface, router

--- shared/modules/usage/usage.router.ts
+++ shared/modules/usage/usage.router.ts
@@ -1,5 +1,5 @@
 import { BaseRouterInstance } from "../../lib/default/decorator";
-import { UsageListRequest, UsageListResponse, UsageStatsRequest, UsageStatsResponse, MyUsageRequest, MyUsageResponse, UsageSessionsRequest, UsageSessionsResponse } from "./usage.interface";
+import { UsageListRequest, UsageListResponse, UsageStatsRequest, UsageStatsResponse, UsageSessionsRequest, UsageSessionsResponse } from "./usage.interface";
 
 export class UsageRouterInstance extends BaseRouterInstance {
     base = "/api";
@@ -7,19 +7,16 @@ export class UsageRouterInstance extends BaseRouterInstance {
     router = [
         { path: "/list", handler: Function },
         { path: "/stats", handler: Function },
-        { path: "/mystats", handler: Function },
         { path: "/sessions", handler: Function },
     ];
 
     list!: (query: UsageListRequest) => Promise<UsageListResponse>;
     stats!: (query: UsageStatsRequest) => Promise<UsageStatsResponse>;
-    mystats!: (query: MyUsageRequest) => Promise<MyUsageResponse>;
     sessions!: (query: UsageSessionsRequest) => Promise<UsageSessionsResponse>;
 
     constructor(inject: Function, functions?: {
         list: (query: UsageListRequest) => Promise<UsageListResponse>,
         stats: (query: UsageStatsRequest) => Promise<UsageStatsResponse>,
-        mystats: (query: MyUsageRequest) => Promise<MyUsageResponse>,
         sessions: (query: UsageSessionsRequest) => Promise<UsageSessionsResponse>,
     }) {
         super();
